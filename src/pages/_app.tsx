1d522c949733808bd3dae1b33d575d4ce6ea6b12
anas-afzal <muhammad.afzal@code.berlin>
2023-10-16 13:47:34 +0200

    feat: create main search page to search artists

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@/boot';
+import { Layout } from '@/components';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ApolloProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
+  );
+};
+
+export default App;
